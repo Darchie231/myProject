bd17363e8765a767c0ad2101de5e3aea32a5849a
darchie231 <dave.archie11@gmail.com>
2024-04-30 14:28:59 +0800

    first commit

---
+++ src/archie/Archie.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package archie;
+
+/**
+ *
+ * @author Rachel
+ */
+public class Archie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
